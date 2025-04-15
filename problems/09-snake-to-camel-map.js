fa69f75878fafe50dad8d101cc11ff34daab7478
SpiritGun91 <spiritgun@vivaldi.net>
2025-04-15 04:17:03 -0400

    Turn the string from snake case to camel case using Array's map method.

--- problems/09-snake-to-camel-map.js
+++ problems/09-snake-to-camel-map.js
@@ -16,13 +16,17 @@ console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
 */
 
 function snakeToCamel(str) {
-    // Your code here
+  // Your code here
+  return str
+    .split("_")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join("");
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
-    module.exports = snakeToCamel;
+  module.exports = snakeToCamel;
 } catch (e) {
-    module.exports = null;
-}
+  module.exports = null;
+}
