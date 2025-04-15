92e153f31051423213a73046635100148eb9d4f6
SpiritGun91 <spiritgun@vivaldi.net>
2025-04-15 05:14:57 -0400

    Return the shortest word in the given string.

--- problems/14-shortest-word.js
+++ problems/14-shortest-word.js
@@ -14,8 +14,18 @@ console.log(shortestWord('do what you enjoy'));         // 'do'
 
 */
 
-let shortestWord = function(sentence) {
+let shortestWord = function (sentence) {
   // Your code here
+  let words = sentence.split(" ");
+  let shortest = words[0];
+
+  words.forEach((word) => {
+    if (word.length <= shortest.length) {
+      shortest = word;
+    }
+  });
+
+  return shortest;
 };
 
 // Your code here
@@ -26,4 +36,4 @@ try {
   module.exports = shortestWord;
 } catch (e) {
   module.exports = null;
-}
+}
