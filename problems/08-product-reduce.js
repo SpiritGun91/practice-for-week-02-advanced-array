7f2568c83f080c11099f04504be09ea62ac2dbd5
SpiritGun91 <spiritgun@vivaldi.net>
2025-04-15 04:13:19 -0400

    Multiply all the numbers in the given array with Array's reduce method.

--- problems/08-product-reduce.js
+++ problems/08-product-reduce.js
@@ -15,9 +15,10 @@ console.log(productWithReduce([4, 3])); // 12
 
 */
 
-let productWithReduce = function(nums) {
-    // Your code here
-}
+let productWithReduce = function (nums) {
+  // Your code here
+  return nums.reduce((acc, num) => acc * num);
+};
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
@@ -25,4 +26,4 @@ try {
   module.exports = productWithReduce;
 } catch (e) {
   module.exports = null;
-}
+}
